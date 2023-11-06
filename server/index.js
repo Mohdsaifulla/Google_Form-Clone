7f2110709a53a0746bd50c6889fad8e048d9d809
Saif <saif49032@gmail.com>
2023-11-06 12:44:11 +0530

    added3

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ server.post("/form-server", async (req, res) => {
   const serverData = req.body;
   const formEntry = new FormEntry(serverData);
   console.log(formEntry);
-  mongoose.set("maxTimeMS", 30000);
+  mongoose.set("maxTimeMS", 50000);
   try {
     const savedEntry = await formEntry.save();
     console.log("Data saved to MongoDB:", savedEntry);
